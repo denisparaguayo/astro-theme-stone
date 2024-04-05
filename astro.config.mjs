eec999b18802f921408fdd5cbdfe6529ee35919c
Denis Cantero <denis.cantero1987@gmail.com>
2024-04-05 11:59:29 -0400

    BASEHEAD

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   viewTransitions: true,
   integrations: [tailwind(), compress()],
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: netlify(),
+  site: 'https://maryprietomodelos.com',
 });
